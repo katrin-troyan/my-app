68fae8241eb8ac18836a92ada7f36ec06707955a
Kateryna Troyan <ket.tro@gmail.com>
2025-11-02 22:57:57 +0200

    add icons and draws

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,5 +3,15 @@ import HidePassIcon from './HidePassIcon';
 import PawIcon from './PawIcon';
 import HeartIcon from './HeartIcon';
 import Label from './Label';
+import CloseIcon from './CloseIcon';
+import ArrowIcon from './ArrowIcon';
 
-export { ViewPassIcon, HidePassIcon, PawIcon, HeartIcon, Label };
+export {
+  ViewPassIcon,
+  HidePassIcon,
+  PawIcon,
+  HeartIcon,
+  Label,
+  CloseIcon,
+  ArrowIcon,
+};
